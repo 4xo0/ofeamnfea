5eefd9ff7c35b912b9773a726b9c08e9e40ae0ec
4xo0 <szymanskifranek23@gmail.com>
2025-07-09 21:26:36 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { EventEmitter } = require("events");
+
+const app = express();
+const events = new EventEmitter();
+const EVENT = "RemoteEvent";
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.post("/executeRequest", (req, res) => {
+  const { username, code } = req.body;
+  if (!username || !code) {
+    console.log("missing user or code");
+    return res.sendStatus(400);
+  }
+
+  console.log(`executed: ${username}: ${code}`);
+  events.emit(EVENT, { username, code });
+  res.sendStatus(200);
+});
+
+app.get("/fetchExecuteRequests", (req, res) => {
+  events.once(EVENT, ({ username, code }) => {
+    res.json({ username, code });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
